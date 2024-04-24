87c0233e3fddf3155dd5e4263257a2badb22e1a3
uni51 <unieye51@gmail.com>
2024-04-24 14:50:29 +0900

    7. shadcn/uiのフォームを使ってフォームのレイアウトを作ろう

--- components/MailForm/MailForm.tsx
+++ components/MailForm/MailForm.tsx
@@ -1,7 +1,42 @@
+"use client";
+
+import { Input } from "../ui/input";
 import React from "react";
+import { Form } from "react-hook-form";
+import { Button } from "../ui/button";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "../ui/form";
 
 const MailForm = () => {
-  return <div>MailForm</div>;
+  return (
+    <Form>
+      <form onSubmit={() => {}} className="space-y-8">
+        <FormField
+          // control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default MailForm;
