dec3ba525f8cda9cea61f5d27f5d96c444e7e575
uni51 <unieye51@gmail.com>
2024-04-27 10:07:45 +0900

    13. useCallbackでカスタムフックス内の関数をメモ化しよう

--- hooks/useMailForm.ts
+++ hooks/useMailForm.ts
@@ -1,5 +1,6 @@
 import { formSchema } from "@/lib/formSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback } from "react";
 import { useForm } from "react-hook-form";
 
 export const useMailForm = () => {
@@ -13,9 +14,9 @@ export const useMailForm = () => {
     },
   });
 
-  const onSubmit = (values: any) => {
+  const onSubmit = useCallback((values: any) => {
     console.log(values);
-  };
+  }, []);
 
   return { form, onSubmit };
 };
